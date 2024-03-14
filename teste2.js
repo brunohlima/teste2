9751dc569ea57af3235e57035bb79d37aae4c0d6
Bruno Henrique <126844317+brunohlima@users.noreply.github.com>
2024-03-13 22:56:31 -0300

    Update teste2.js

--- teste2.js
+++ teste2.js
@@ -6,7 +6,7 @@ const {chromium} = require('playwright');
     const page = await browser.newPage()
     await page.goto('https://react-redux.realworld.io/#/login')
 
-    await page.fill('input[type = "email"]', 'nobrulg21@gmail.com')
+    await page.fill('input[type = "email"]', 'teste@gmail.com')
     await page.fill('input[type = "password"]', '12345')
     await page.click('form >> "Sign in"', {position: {x: 0, y: 0}, button: 'left', modifiers: ['Shift'], force: true, timeout: 45000})
     await page.focus('form >> "Sign in"')
